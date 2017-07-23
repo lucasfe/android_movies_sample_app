520b8a3bc89d6b51d9ff4df8e0fabb13b57ec118
Lucas Ferreira <lucasfe@gmail.com>
2017-07-23 20:22:05 -0300

    Init project and add Recycle view for posters
    Signed-off-by: Lucas Ferreira <lucasfe@gmail.com>

---
+++ app/src/main/java/br/com/campuscode/movies/MainActivity.java
@@ -0,0 +1,50 @@
+package br.com.campuscode.movies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.campuscode.movies.adapters.MoviesAdapter;
+
+public class MainActivity extends AppCompatActivity implements MoviesAdapter.ItemClickListener {
+
+
+    MoviesAdapter adapter;
+
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+            List<String> urls = new ArrayList<>();
+            urls.add("www.teste.com");
+            urls.add("www.teste.com");
+            urls.add("www.teste.com");
+            urls.add("www.teste.com");
+            urls.add("www.teste.com");
+            urls.add("www.teste.com");
+            urls.add("www.teste.com");
+
+
+            // set up the RecyclerView
+            RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_movies);
+            int numberOfColumns = 2;
+            recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+            adapter = new MoviesAdapter(this, urls);
+            adapter.setClickListener(this);
+            recyclerView.setAdapter(adapter);
+
+        }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "Testing Recycler view", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/br/com/campuscode/movies/adapters/MoviesAdapter.java
@@ -0,0 +1,76 @@
+package br.com.campuscode.movies.adapters;
+
+import android.content.Context;
+import android.media.Image;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+import br.com.campuscode.movies.R;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder>{
+
+    private List<String> mImagesUrls;
+    private Context mContext;
+    private LayoutInflater inflater;
+    ItemClickListener mClickListener;
+
+    public MoviesAdapter(Context context, List<String> imagesUrls) {
+        mImagesUrls = imagesUrls;
+        mContext = context;
+        inflater = LayoutInflater.from(mContext);
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.movies_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.imageView.setImageDrawable(mContext.getDrawable(R.drawable.moonlight));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImagesUrls.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public ImageView imageView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.iv_movie_poster);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Log.v("TAG", "Teste");
+        }
+    }
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+
+
+}
